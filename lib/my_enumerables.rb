cdd762f40756d35996f6067f994d995c08bf2427
rahmanagha <aghazad.rahman@gmail.com>
2026-04-28 10:02:02 +0400

    feat: Define my_each on Array using a while loop
    - Iterate over each element with a manual index and yield it to the block
    - Return self to match the contract of Ruby's built‑in each
    - Serves as the foundation for the custom Enumerable module
    - Does not rely on any other iteration method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,13 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+
+  def my_each
+  index = 0
+    while index < self.length
+      yield(self[index])
+    index += 1
+    end
+    self
+  end
 end
