941ba9ea06fec02455ea88cc5834a819abbf0192
rahmanagha <aghazad.rahman@gmail.com>
2026-04-28 10:29:15 +0400

    feat: Add my_all? to Enumerable module
    - Use self.my_each to iterate over the collection
    - Return false immediately if any element fails the block test
    - Return true if all elements pass
    - Return an Enumerator when no block is given, using to_enum(:my_all?)

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,13 @@
 module Enumerable
   # Your code goes here
+  def my_all?
+    return to_enum(:my_all?) unless block_given?
+
+    self.my_each do |elem|
+      return false unless yield(elem)
+    end
+    true
+  end
 end
 
 # You will first have to define my_each
